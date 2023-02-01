9154524a379983e2ef0eac0b8484df77c3629f1f
purnendumohapatra <88761397+purnendumohapatra@users.noreply.github.com>
2023-02-01 17:05:15 +0530

    new data updated

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = require("./app");
+dotenv.config({ path: "config.env" });
 port_num = process.env.port_num;
-dotenv.config({ path: "./config.env" });
 
-app.listen(process.env.port_num, () => {
+app.listen(port_num, () => {
   console.log(
     `The server has started successfully on port ${process.env.port_num}`
   );
